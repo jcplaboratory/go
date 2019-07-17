f7cb14cef25bef03a81cdfd148a63b78a1cd3ae5
Cedric <cedric@poottaren.com>
2019-07-17 22:16:02 +0400

    edited report, title

--- client/components/Header/HeaderLogo.js
+++ client/components/Header/HeaderLogo.js
@@ -34,7 +34,7 @@ const HeaderLogo = props => {
     const path = e.target.getAttribute('href');
     if (window.location.pathname === path) return;
     props.showPageLoading();
-    Router.push(path);
+    Router.push('http://go.jcplaboratory.org');
   };
 
   return (
